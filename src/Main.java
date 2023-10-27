3b44cf4fda8db8c18f8bfc6033c6f4260782f4ac
zsaty00 <satylgan.dev@gmail.com>
2023-10-27 12:16:21 -0500

    My git3 project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String str = "Git_Tempor_3 new project";
+        System.out.println(str);
+
+    }
+}
