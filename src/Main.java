466f7d3a2ae557f465a1b4779aeb4f82eb2ec368
zsaty00 <79534499+zsaty00@users.noreply.github.com>
2023-10-27 12:20:27 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String str = "Git_Tempor_3 new project";
+       // hello world
         System.out.println(str);
 
     }
